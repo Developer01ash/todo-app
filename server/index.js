802504337d4276aaef1762c5b4c61640f51701e8
Developer01ash <vermashutosh01@gmail.com>
2024-01-25 18:33:28 +0530

    add cors

--- server/index.js
+++ server/index.js
@@ -12,7 +12,18 @@ const routes = require("./routes");
 const app = express();
 const PORT = 8080;
 
-app.use(cors({ origin: "*" }));
+const allowlist = ["https://todo-app-nu-bay.vercel.app"];
+const corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (allowlist.indexOf(req.header("Origin")) !== -1) {
+    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false }; // disable CORS for this request
+  }
+  callback(null, corsOptions); // callback expects two parameters: error and options
+};
+
+app.use(cors(corsOptionsDelegate));
 
 app.use(
   bodyParser.urlencoded({
